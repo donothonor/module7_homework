2b906a31c46f943ff7f946789a4874a77be5e58b
donothonor <nagizov2@gmail.com>
2021-07-28 19:03:10 +0400

    complete all tasks

---
+++ Задание 5.js
@@ -0,0 +1,34 @@
+class Device {
+    constructor(name, power) {
+        this.name = name
+        this.power = power || null
+    }
+    getPower(power) {
+        if (power !== null) {
+            console.log(`Power is ${power}`)
+        } else {
+            console.log(`Device is off`)
+        }
+    }
+}
+
+const lamp = new Device('Lamp', 5)
+console.log(lamp)
+
+class ComputerDevice extends Device {
+    constructor(brand, name, power) {
+        super(name, power)
+        this.brand = brand
+        this.getPower = function (power) {
+            if (power !== null) {
+                console.log(`${brand} power is ${power}`)
+            } else {
+                super.getPower()
+            }
+        }
+    }
+}
+
+
+
+
